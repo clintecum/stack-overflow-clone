34a576b119db929d6f568b8179ce14537973b297
Hola! Yo soy Llama & clintecum & Zach Harris <pair+backstreetsoy.clintecum.z-lake@devbootcamp.com>
2015-04-10 13:28:25 -0500

    working validations?

--- stack_city/app/models/user.rb
+++ stack_city/app/models/user.rb
@@ -9,8 +9,6 @@ class User < ActiveRecord::Base
   validates :username, :email, :password, presence: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 
-  # def voted_once
-  #   #if a user has voted on either a comment, a question, or and answer one time already, they can not vote again.
-  # end
+
 
 end
