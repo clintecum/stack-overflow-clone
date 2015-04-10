6b0968a1c856ca87efaea78ddf5a7259c4454754
z-lake <zachary.austin.harris@gmail.com>
2015-04-10 14:38:26 -0500

    finished routes

--- stack_city/config/routes.rb
+++ stack_city/config/routes.rb
@@ -2,31 +2,20 @@ Rails.application.routes.draw do
 
   root 'questions#index'
 
-  scope shallow_prefix: "user" do
     resources :users do
-      resources :questions, shallow: true
-      resources :answers, shallow: true
-      resources :comments, shallow: true
-      resources :votes, shallow: true
-      resources :favorites, shallow: true
+      resources :questions, only: :index
+      resources :answers, only: :index
+      resources :comments, only: :index
+      resources :votes, only: :index
+      resources :favorites, only: :index
     end
-  end
 
-  concern :commentable do
-    resources :comments
-  end
-
-  concern :voteable do
-    resources :votes
-  end
-
-  concern :favorable  do
-    resources :favorites
-  end
-
-  scope shallow_prefix: "hidden" do
-    resources :questions, concerns: [:commentable, :voteable, :favorable] do
-      resources :answers, concerns: [:commentable, :voteable], shallow: true
-    end
-  end
+    resources :questions 
 end
+
+# /questions/id 
+# /questions
+# /quesitons/new
+# /quesitons/edit
+# /questions/id/answers/
+
