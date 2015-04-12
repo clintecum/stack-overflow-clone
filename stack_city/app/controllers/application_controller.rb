c25d33c49a95120c610b68257bed853f8061950f
Hola! Yo soy Llama & clintecum & Zach Harris <pair+backstreetsoy.clintecum.z-lake@devbootcamp.com>
2015-04-12 18:58:31 -0500

    changes blah

--- stack_city/app/controllers/application_controller.rb
+++ stack_city/app/controllers/application_controller.rb
@@ -2,19 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-<<<<<<< HEAD
+
 
   private
   def current_user
     @_current_user ||= session[:current_user_id] &&
       User.find_by(id: session[:current_user_id])
   end
-  
+
   def user_not_authorized
     flash[:error] = "You don't have access to this section."
     redirect_to :back
   end
-=======
+
   include SessionsHelper
->>>>>>> 9b00096a14afbec0c1622e9ddc72ac58ebc7d9b9
+
 end
--- stack_city/app/helpers/sessions_helper.rb
+++ stack_city/app/helpers/sessions_helper.rb
@@ -1,8 +1,5 @@
 module SessionsHelper
-<<<<<<< HEAD
 
-=======
->>>>>>> 9b00096a14afbec0c1622e9ddc72ac58ebc7d9b9
   def log_in(user)
     session[:user_id] = user.id
   end
@@ -11,24 +8,20 @@ module SessionsHelper
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
-<<<<<<< HEAD
   def logged_in?
     !current_user.nil?
   end
 
-=======
->>>>>>> 9b00096a14afbec0c1622e9ddc72ac58ebc7d9b9
   def log_out
     session.delete(:user_id)
     @current_user = nil
   end
-<<<<<<< HEAD
+
 
   def authorize
     redirect_to '/login' unless current_user
   end
 
 end
-=======
-end
->>>>>>> 9b00096a14afbec0c1622e9ddc72ac58ebc7d9b9
+
+
